5ec980365a3f792744b62828121a8057a15b1596
Dhruv-2003 <dhruvagarwal2017@gmail.com>
2024-02-09 22:01:03 +0530

    feat: basic frame methods determined

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,24 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta
+          name="og:image"
+          content="https://i.ibb.co/rsrJbFg/Screenshot-2024-02-08-at-6-06-22-PM.png"
+        />
+        <meta name="fc:frame" content="vNext" />
+        <meta
+          name="fc:frame:image"
+          content="https://i.ibb.co/rsrJbFg/Screenshot-2024-02-08-at-6-06-22-PM.png"
+        />
+        <meta
+          name="fc:frame:post_url"
+          content={`${process.env.NEXT_PUBLIC_HOST}/api/frame`}
+        />
+        <meta name="fc:frame:input:text" content="Safe Address: zkevm:0x..." />
+        <meta name="fc:frame:button:1" content="Create Frame" />
+        <meta name="fc:frame:button:1:action" content="post" />
+      </Head>
       <body>
         <Main />
         <NextScript />
--- pages/api/frame.ts
+++ pages/api/frame.ts
@@ -1,39 +1,63 @@
-import { Frame, getFrameHtml, validateFrameMessage } from "frames.js";
+import {
+  Frame,
+  getFrameHtml,
+  getFrameMessage,
+  validateFrameMessage,
+} from "frames.js";
+import { NextApiRequest, NextApiResponse } from "next";
 import { NextRequest } from "next/server";
 
-export async function POST(request: NextRequest) {
-  const body = await request.json();
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const body = await req.body;
+  // console.log(req);
+  // console.log(body);
 
   // Parse and validate the frame message
-  const { isValid, message } = await validateFrameMessage(body);
-  if (!isValid || !message) {
-    return new Response("Invalid message", { status: 400 });
-  }
+  // try {
+  // const { isValid, message } = await validateFrameMessage(body);
+  // if (!isValid || !message) {
+  //   return new Response("Invalid message", { status: 400 });
+  // }
 
-  const randomInt = Math.floor(Math.random() * 100);
-  const imageUrlBase = `https://picsum.photos/seed/${randomInt}`;
+  const frameMessage = await getFrameMessage(body);
+  console.log(frameMessage);
+
+  const imageUrlBase = `${process.env.NEXT_PUBLIC_HOST}/api/og`;
+  const safeAddress = frameMessage.inputText;
 
   // Use the frame message to build the frame
   const frame: Frame = {
     version: "vNext",
-    image: `${imageUrlBase}/1146/600`,
+    image: `${imageUrlBase}/${safeAddress}`,
     buttons: [
       {
-        label: `Next (pressed by ${message.data.fid})`,
+        label: `Next`,
         action: "post",
       },
     ],
-    ogImage: `${imageUrlBase}/600`,
-    postUrl: `${process.env.NEXT_PUBLIC_HOST}/frames`,
+    ogImage: `${imageUrlBase}/${safeAddress}`,
+    postUrl: `${process.env.NEXT_PUBLIC_HOST}/api/frame`,
   };
+  console.log(frame);
 
   // Return the frame as HTML
   const html = getFrameHtml(frame);
 
-  return new Response(html, {
-    headers: {
-      "Content-Type": "text/html",
-    },
-    status: 200,
-  });
+  // return new Response(html, {
+  //   headers: {
+  //     "Content-Type": "text/html",
+  //   },
+  //   status: 200,
+  // });
+
+  res.status(200).send(html);
+  // } catch (err) {
+  //   console.log(err);
+  // }
 }
+
+export const dynamic = "force-dynamic";
+// export const runtime = "edge";
